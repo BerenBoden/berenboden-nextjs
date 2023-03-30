608b053d71ecf0d970bca22e9a7360d7c5a95d92
berenboden <beren.boden@gmail.com>
2023-03-30 20:59:41 +1300

    Initial setup for Next.JS application configured with TypeScript using create next app and tailwindcss. Modified index.ts for initial main content. Created two boilerplate components (HeroSection, Showcase)

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export type Resource = {
+  id: number;
+  title: string;
+  description: string;
+  category: Category;
+  image: string;
+  slug: string;
+  date: string;
+  author: Author;
+  html: any;
+};
+
+export type ShowcaseProps = {
+  posts: Resource[];
+};
+
+export type Category = {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+};
+
+export type Author = {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+};
