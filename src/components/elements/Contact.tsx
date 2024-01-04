c834d92bfaba0f18abbc366ae792da4d2761aee6
berenboden <beren.boden@gmail.com>
2024-01-04 21:39:17 +1300

    added pagination component to all resource pages

--- src/components/elements/Contact.tsx
+++ src/components/elements/Contact.tsx
@@ -44,7 +44,7 @@ export default function Contact() {
                   htmlFor="first-name"
                   className="block text-sm font-semibold leading-6 text-gray-900"
                 >
-                  First name
+                  Name
                 </label>
                 <div className="mt-2.5">
                   <input
@@ -61,7 +61,7 @@ export default function Contact() {
                   htmlFor="last-name"
                   className="block text-sm font-semibold leading-6 text-gray-900"
                 >
-                  Last name
+                  Business
                 </label>
                 <div className="mt-2.5">
                   <input
@@ -69,38 +69,6 @@ export default function Contact() {
                     name="last-name"
                     id="last-name"
                     autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="budget"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Budget
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    id="budget"
-                    name="budget"
-                    type="text"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="website"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Website
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="url"
-                    name="website"
-                    id="website"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,37 +1,47 @@
 import Resource from "@/components/elements/Resource";
 import { Resource as ResourceType } from "@/types";
-import { GetStaticPropsContext } from "next";
+import { Pagination } from "@/components/elements/Pagination";
+import { GetServerSidePropsContext } from "next";
 
-export default function certifications({
-  resources,
+export default function articles({
+  docs,
+  totalDocs,
 }: {
-  resources: ResourceType[];
+  docs: ResourceType[];
+  totalDocs: number;
 }) {
   return (
     <div className="mx-auto max-w-7xl px-6">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 py-6">
-        {resources.map((resource) => {
-          return (
-            <div className="">
-              <div className="" key={resource.id}>
-                <Resource resource={resource} />
+        {docs.length === 0 ? (
+          <div>Nothing found...</div>
+        ) : (
+          docs.map((resource) => {
+            return (
+              <div className="">
+                <div className="" key={resource.id}>
+                  <Resource resource={resource} />
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
+      <Pagination totalDocs={totalDocs} />
     </div>
   );
 }
 
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { page = 1, limit = 16 } = context.query; // Set default values
   const data = await fetch(
-    `${process.env.API_URL}/api/resources/featured/article`
+    `${process.env.API_URL}/api/resources/type/article?page=${page}&limit=${limit}`
   );
-  const resources = await data.json();
+  const { docs, totalDocs } = await data.json();
   return {
     props: {
-      resources,
+      docs,
+      totalDocs,
     },
   };
 }
